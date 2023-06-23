1007bd9b4d0640f2634780e4ebd0cd9814739012
Caputommy <caputommy@gmail.com>
2023-06-24 00:00:07 +0200

    Added method signatures in FollowMeAppCOntroller, like onLoadEnvironmentCommand and shpwNewSimulationSetting

--- app/src/main/java/it/unicam/cs/followme/app/FollowMeAppController.java
+++ app/src/main/java/it/unicam/cs/followme/app/FollowMeAppController.java
@@ -5,10 +5,16 @@ import it.unicam.cs.followme.model.FollowMeLabel;
 import it.unicam.cs.followme.model.environment.SurfacePosition;
 import it.unicam.cs.followme.model.items.Robot;
 import it.unicam.cs.followme.model.simulation.SignalingItemSimulationExecutor;
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * JavaFX Controller of FollowMeApp.
@@ -71,14 +77,13 @@ public class FollowMeAppController {
     private final Controller<SurfacePosition, FollowMeLabel, Robot<SurfacePosition, FollowMeLabel>> controller =
             Controller.getFollowMeController();
 
-    //private final Image stopImage = new Image("app/src/main/resources/icons/PlayIcon.png");
-    //private final Image playImage = new Image("app/src/main/resources/icons/StopIcon.png");
+    private final Image stopImage = new Image("/icons/StopIcon.png");
+    private final Image playImage = new Image("/icons/PlayIcon.png");
 
 
     public void initialize() {
         initSpinners();
         initZoomSlider();
-        //simulationStateIcon.setImage(playImage);
     }
 
     private void initSpinners() {
@@ -89,13 +94,13 @@ public class FollowMeAppController {
 
     private void initPaceTimeSpinner() {
         paceTimeSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(
-                0.1, Double.POSITIVE_INFINITY,
+                0.1, Double.MAX_VALUE,
                 SignalingItemSimulationExecutor.DEFAULT_INSTRUCTION_PACE_TIME, 0.1));
     }
 
     private void initPlaySecondsSpinner() {
         playSecondsSpinner.setValueFactory(new SpinnerValueFactory.DoubleSpinnerValueFactory(
-                0.01, Double.POSITIVE_INFINITY,
+                0.01, Double.MAX_VALUE,
                 SignalingItemSimulationExecutor.DEFAULT_INSTRUCTION_PACE_TIME, 0.01));
     }
 
@@ -108,4 +113,50 @@ public class FollowMeAppController {
         zoomViewSlider = new Slider(0, 10, 5);
     }
 
+    /**
+     * Resets the stopwatch and clears the robot map.
+     * This method is invoked whenever the current simulation execution is reset due to the change of
+     * one of its core settings.
+     */
+    private void showNewSimulationSetting() {
+        //Set stopwatch
+        //TODO
+        //Clear shown robots
+        //TODO
+    }
+
+    /**
+     * Resets the shown program source code.
+     */
+    private void refreshProgramCode() {
+        programTextArea.clear();
+        programTextArea.appendText(controller.getCurrentSourceCode());
+    }
+
+
+    /**
+     * Method used to handle the load environment command.
+     *
+     * @param event the triggering event.
+     */
+    @FXML
+    private void onLoadEnvironmentCommand(Event event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Load Environment File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Txt Files", "*.txt"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File selectedFile = fileChooser.showOpenDialog(((Node) event.getSource()).getScene().getWindow());
+        if (selectedFile != null) {
+            try {
+                controller.openEnvironment(selectedFile);
+                showNewSimulationSetting();
+                //TODO drawEnvironment()
+            } catch (IOException e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error...");
+                alert.setHeaderText(e.getMessage());
+            }
+        }
+    }
 }
